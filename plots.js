7171e92bbdd93d14f7070914c229f8438fad687c
Benjamen Alford <benjamen.alford@gmail.com>
2024-06-17 21:05:43 -0500

    v1

---
+++ plots.js
@@ -0,0 +1,52 @@
+// Create an array of each country's numbers
+let australia = Object.values(country_data.australia);
+
+// Create an array of category labels
+let labels = Object.keys(country_data.australia);
+
+// Display the default plot
+function init() {
+
+  // Default trace for the country data
+  let trace = {
+    values: Object.values(country_data.australia),
+    labels: labels,
+    type: "pie",
+    sort: false // Ensure sectors are not reordered
+  }
+
+  // Data Array
+  let data = [trace]
+
+  // Layout object
+  layout = {}
+
+  // Render the plot to the div tag with id "pie"
+  Plotly.newPlot("pie", data, layout);
+}
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  // Use D3 to select the dropdown menu
+  let dropdownMenu = d3.select("#selDataset");
+
+  // Assign the value of the dropdown menu option to a variable
+  let selectedData = dropdownMenu.property('value')
+  console.log(selectedData)
+  // Initialize an empty array for the new country's data
+  let newData = Object.values(country_data[selectedData])
+  console.log(newData)
+
+  // Call function to update the chart
+  Plotly.restyle("pie", "values", [newData])
+}
+
+// Update the restyled plot's values
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
+
+init();
